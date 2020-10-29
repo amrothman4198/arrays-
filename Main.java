9a7865dc8f945939f07c98d14b9ceb5c60ce9942
othmanamr <amr.othman@patelhs.org>
2020-10-29 14:46:37 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+class Main {
+  public static void main(String[] args){
+  int[ ] grades= {99,98,98,88,68,100};
+
+
+ String[ ] names = {"James", "Nic", "Chris","lebron",  "cash", "Amr"};
+
+ System.out.println(names[1] + " has a grade of " + grades[2]);
+ System.out.println(names[4] + " has a grade of " + grades[3]);
+ System.out.println(names[0] + " has a grade of " + grades[0]);
+ System.out.println(names[2] + " has a grade of " + grades[1]);
+ System.out.println(names[3] + " has a grade of " + grades[4]);
+ System.out.println(names[5] + " has a grade of " + grades[5]);
+
+ String[] grades = new String[];
+
+
+
+  }
+}
